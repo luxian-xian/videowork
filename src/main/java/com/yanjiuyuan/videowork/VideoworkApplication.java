cda0715c953ef65f397d1e52df45cc29ae5d4ff3
luxian-xian <youmeiluxian@163.com>
2020-09-09 16:45:47 +0800

    第一次提交

---
+++ src/main/java/com/yanjiuyuan/videowork/VideoworkApplication.java
@@ -0,0 +1,13 @@
+package com.yanjiuyuan.videowork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yanjiuyuan/videowork/VideoworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yanjiuyuan.videowork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
